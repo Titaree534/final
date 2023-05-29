ef7d1c94ced813badf7a9e8f230fdff69f7c8b28
Titaree534 <124623514+Titaree534@users.noreply.github.com>
2023-05-29 11:23:03 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,155 @@
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "oo pasta",
+          "type": "food",
+          "level": "⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.50000500000186,
+            13.765181336744632
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สุกี้ตี๋น้อย",
+          "type": "food",
+          "level": "⭐⭐⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.57137999999873,
+            13.716901338464524
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ร้านอาหารตึกใบหยก",
+          "type": "food",
+          "level": "⭐⭐⭐⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.54061650000324,
+            13.754238758316731
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ร้านเจ๊โอ",
+          "type": "food",
+          "level": "⭐⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.52256572883664,
+            13.742365337557331
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "คาเฟ่อเมซอน",
+          "type": "food",
+          "level": "⭐⭐⭐⭐⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.5332732500022,
+            13.744836712471468
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "บัวลอยปริญญา",
+          "type": "food",
+          "level": "⭐⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.52296299999995,
+            13.743049337534543
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ข้าวมันไก่เจ๊โบว์",
+          "type": "food",
+          "level": "⭐⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.52283698848362,
+            13.743260441268689
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ร้านเชฟเอียน",
+          "type": "food",
+          "level": "⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.55205372883586,
+            13.720521183699475
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ครัวคลองเตยซีฟู้ด",
+          "type": "food",
+          "level": "⭐⭐⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.56120999999621,
+            13.716453338480392
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ก๋วยจั๊บนายใช้",
+          "type": "food",
+          "level": "⭐⭐⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.5229700000001,
+            13.72501933817648
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
